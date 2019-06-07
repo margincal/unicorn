7335cd381750f7415d28171373dc34ac3092a73b
margincal <51220664+margincal@users.noreply.github.com>
2019-06-07 14:54:19 -0400

    Add files via upload

---
+++ aggService/beans/MarginCalBean.java
@@ -0,0 +1,29 @@
+package com.broadridge.unicorn.aggService.beans;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class MarginCalBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2724622167389383608L;
+
+	private double regT;
+	private double house;
+	private double maintenance;
+	private double buyPower;
+	private double nyseReq;
+	private double houseReq;
+}
